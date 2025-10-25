efb608023ae51031356482d16cfa145808f53ebc
unknown <gabsiwajd@gmail.com>
2025-10-25 04:37:23 +0100

    connect

--- fertilization/main.py
+++ fertilization/main.py
@@ -3,10 +3,22 @@ from datetime import datetime
 from models.data_models import RecommendationRequest, RecommendationResponse, HistoryRecord
 from models.fertilization_model import TunisianOliveFertilizationModel
 from db.history_store import add_history, get_tree_history, get_all_history, delete_tree_history
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="Tunisian Olive Tree Fertilization Recommendation System")
 fertilization_model = TunisianOliveFertilizationModel()
+origins = [
+    "http://localhost:3000",  # React dev server
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080","http://127.0.0.1:8000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.post("/recommend", response_model=RecommendationResponse)
 async def get_recommendation(request: RecommendationRequest):
     try:
@@ -62,6 +74,14 @@ async def root():
         "version": "1.0",
         "description": "AI-powered recommendations for olive fertilization in Tunisia",
     }
+# ✅ Add these endpoints
+@app.get("/locations")
+def get_locations():
+    return {"locations": ["Sousse","monastir","tunis","beja", "Sfax", "Kairouan", "Gabes", "Bizerte"]}
+
+@app.get("/varieties")
+def get_varieties():
+    return {"varieties": ["Chemlali", "Chetoui", "Koroneiki", "Arbequina"]}
 
 if __name__ == "__main__":
   # FIXED: Was _name and main
